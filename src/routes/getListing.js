a4e55b0bec8125e5936eb8ae5e25cedd8ea1844c
Alexsander Campos <ext-alexsander.boas@bbts.com.br>
2026-01-29 15:08:48 -0300

    Errors return in hapi using hapi/boom

--- src/routes/getListing.js
+++ src/routes/getListing.js
@@ -1,10 +1,13 @@
 import { fakeListings } from "./fake-data";
+import Boom from "@hapi/boom";
 
 export const getListingRoute = {
   method: "GET",
   path: "/api/page-list/{id}",
   handler: (req, h) => {
     const id = req.params.id;
-    return fakeListings.find((listing) => listing.id === id);
+    const listings = fakeListings.find((listing) => listing.id === id);
+    if (!listings) throw Boom.notFound(`Listing does not exist with id ${id}`);
+    return listings;
   },
 };
