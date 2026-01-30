2a3bb10d0f8683db39cfeeb55427748b2469f521
acvb-alexsander <acvb.developer@gmail.com>
2026-01-30 10:51:30 -0300

    connect to a MySQL database from node.js

--- src/server.js
+++ src/server.js
@@ -1,14 +1,17 @@
 import Hapi from "@hapi/hapi";
 import routes from "./routes";
+import { db } from "./dbase";
+
+let server;
 
 const start = async () => {
-  const server = Hapi.server({
+  server = Hapi.server({
     port: 8000,
     host: "localhost",
   });
 
   routes.forEach((route) => server.route(route));
-
+  db.connect();
   await server.start();
   console.log(`Server is listening on ${server.info.uri}`);
 };
@@ -17,5 +20,15 @@ process.on("unhandledRejection", (err) => {
   console.log(err);
   process.exit(1);
 });
+
+process.on('SIGINT', () =>{
+  console.log('Stopping server...');
+
+  await.server.stop({timeout:10000});
+  db.end();
+  console.log('Server stopped');
+  process.exit(0);
+  
+})
 
 start();
