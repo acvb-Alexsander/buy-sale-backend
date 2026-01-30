67324cc32a3c54041b19e7c40e37e3f27e2d0bf8
acvb-alexsander <acvb.developer@gmail.com>
2026-01-30 13:20:09 -0300

    making sqlwork

--- src/routes/getAllListing.js
+++ src/routes/getAllListing.js
@@ -3,7 +3,10 @@ import { fakeListings } from "./fake-data";
 export const getAllListingRoute = {
   method: "GET",
   path: "/api/page-list",
-  handler: (req, h) => {
-    return fakeListings;
+  handler:async (req, h) => {
+    const {results} = await db.query(
+      'SELECT * FROM listings'
+    );
+    return results;
   },
 };
